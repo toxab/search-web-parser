fc71707127d9ba2feca4ac69643cdd67f5f69e4f
toxab <tohub888@gmail.com>
2020-04-14 23:14:54 +0300

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Api\BaseApiController;
-use DiDom\Document;
 use Illuminate\Contracts\Support\Renderable;
 
 class HomeController extends BaseApiController
--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -10,70 +10,12 @@ use Illuminate\Http\Request;
 
 class ParserController extends BaseApiController
 {
-    
     /**
      * Display a listing of the resource.
      * @return JsonResponse
      */
     public function index()
     {
-        $str = '<!DOCTYPE html>
-        <html>
-         <head>
-           <title>!DOCTYPE</title>
-           <meta charset="utf-8">
-         </head>
-         <body>
-         <div id="container">
-         <div class="asd">
-
-          <p>111Разум — это Будда, а прекращение умозрительного мышления — это путь.
-          Перестав мыслить понятиями и размышлять о путях существования и небытия,
-          о душе и плоти, о пассивном и активном и о других подобных вещах,
-          начинаешь осознавать, что разум — это Будда,
-          что Будда — это сущность разума,
-          и что разум подобен бесконечности.</p>
-          </div>
-          <div>
-          <p>222Разум — это Будда, а прекращение умозрительного мышления — это путь.
-          Перестав мыслить понятиями и размышлять о путях существования и небытия,
-          о душе и плоти, о пассивном и активном и о других подобных вещах,
-          начинаешь осознавать, что разум — это Будда,
-          что Будда — это сущность разума,
-          и что разум подобен бесконечности.</p>
-          </div>
-          <div>
-          <p>333Разум — это Будда, а прекращение умозрительного мышления — это путь.
-          Перестав мыслить понятиями и размышлять о путях существования и небытия,
-          о душе и плоти, о пассивном и активном и о других подобных вещах,
-          начинаешь осознавать, что разум — это Будда,
-          что Будда — это сущность разума,
-          и что разум подобен бесконечности.</p>
-          </div>
-        </div>
-
-         </body>
-        </html>';
-    
-        $url = 'https://www.google.com/search?q=kroliki.ru/&oq=kroliki.ru/&num=10';
-        $document = new Document($url, true);
-        $searchLinks = $document->find('div#main')[0]->children();
-        $searchLinks = array_splice($searchLinks, 3, 10);
-        foreach ($searchLinks as $k => $v) {
-            $kTemp = $k;
-            $lastChild = $v->lastChild();
-            $classNode = $v->cloneNode();
-            $node = $v->getNode();
-            $attr = $v->attr('div');
-            $nextS = $v->nextSibling();
-            $nextAllS = $v->nextSiblings();
-            $childNodes = $v->children();
-            $closest = $v->closest('#rso');
-            $text = $v->text();
-            $clases = $v->classes();
-        
-        }
-    
         return $this->getResponse([], 'index parser');
     }
     
